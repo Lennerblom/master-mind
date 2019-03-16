949b66e761392ffee2612d817def96185407da48
michael lennerblom <mlennerblom@gmail.comgit config --listgit config --global user.name Michael Lennerblomgit config --global user.email mlennerblom@gmail.comgit config --listgit config --global user.name Michael>
2019-03-16 13:29:55 -0700

    Updates

---
+++ precache-manifest.4b8d551e75fddbe2b06c23dad36f7c4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1fd0394c71c5eeafa50f6b7c49c88c1",
+    "url": "/master-mind/static/media/wood-wallpapers.a1fd0394.jpg"
+  },
+  {
+    "revision": "1ddd3b962cc9ece4d199",
+    "url": "/master-mind/static/js/runtime~main.7d8a37f8.js"
+  },
+  {
+    "revision": "901706ee408eabfc1431",
+    "url": "/master-mind/static/js/main.e256a519.chunk.js"
+  },
+  {
+    "revision": "64c6a8a90bd8e0c7278d",
+    "url": "/master-mind/static/js/2.8b717640.chunk.js"
+  },
+  {
+    "revision": "901706ee408eabfc1431",
+    "url": "/master-mind/static/css/main.c2e439c8.chunk.css"
+  },
+  {
+    "revision": "d12c00281c2066934b841aa9a1760065",
+    "url": "/master-mind/index.html"
+  }
+];
