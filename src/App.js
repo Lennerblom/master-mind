1c9a1b2536065fa795f3cb16b0dad56256a2a50c
michael lennerblom <mlennerblom@gmail.comgit config --listgit config --global user.name Michael Lennerblomgit config --global user.email mlennerblom@gmail.comgit config --listgit config --global user.name Michael>
2019-03-19 20:14:55 -0700

    changed title font

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Game from './game';
+//import Login from './login';
 import './App.scss';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        {/* <Login/> */}
         <Game/>
       </div>
     );
